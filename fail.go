800d1fed549208f39d64b4fc5400f30b4e5907cd
IuryAlves <iury.alves@voiapp.io>
2022-01-20 17:28:43 +0100

    Fail go binary

--- fail.go
+++ fail.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"os/exec"
 )
 
 func main() {
-	exec.Command("terraform", "plan", "--fail")
+	cmd := exec.Command("terraform", "plan", "--fail")
+	_, err := cmd.Output()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "❌\u00A0 %v\n", err)
+		os.Exit(1)
+	}
 }
